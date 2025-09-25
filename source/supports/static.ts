20db7290bc78b9e457217691a1d483ff2f3d84c4
Protorians <129060085+protorians@users.noreply.github.com>
2025-09-25 18:35:09 +0000

    Refine `StaticParameter` class by adding type annotations, detailed JSDoc comments, method enhancements (`cloneOriginal`), and ensuring immutability with `readonly` properties.

--- source/supports/static.ts
+++ source/supports/static.ts
@@ -1,9 +1,17 @@
 import type {IParametersSet, IStaticParameters, IStaticProps} from "@/types";
 
 
+/**
+ * Represents a collection of static parameters that can be managed through a set-like interface.
+ * This class allows adding, removing, resetting, and cloning of the parameters while retaining
+ * the initial state for reference.
+ *
+ * @template T A type that extends `IStaticProps<T>` to ensure type compatibility.
+ * @implements {IStaticParameters<T>}
+ */
 export class StaticParameter<T extends IStaticProps<T>> implements IStaticParameters<T> {
 
-    stack: IParametersSet<T>;
+    readonly stack: IParametersSet<T>;
 
     constructor(
         public readonly initial: IStaticProps<T>,
@@ -16,15 +24,19 @@ export class StaticParameter<T extends IStaticProps<T>> implements IStaticParame
         return [...this.stack.values()] as T[];
     }
 
+    /**
+     * Initializes the current instance by iterating through the `initial` collection
+     * and adding its values using the `add` method.
+     * @return {this} The current instance for method chaining.
+     */
     protected initialize(): this {
-        for (const value of this.initial) {
+        for (const value of this.initial)
             this.add(value);
-        }
         return this;
     }
 
     get value(): T | undefined {
-        return this.stack.values().next().value || undefined;
+        return [...this.stack.values()][0] || undefined;
     }
 
     add(value: T): this {
@@ -52,5 +64,9 @@ export class StaticParameter<T extends IStaticProps<T>> implements IStaticParame
 
     clone(): this {
         return new (this.constructor as any)([...this.initial]);
+    }
+
+    cloneOriginal(): this {
+        return new (this.constructor as any)([...this.stack.entries()]);
     }
 }
